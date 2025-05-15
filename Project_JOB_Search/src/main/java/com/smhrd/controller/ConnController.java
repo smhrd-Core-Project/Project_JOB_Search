231b0b871002d89fa5d0e5b0f8e7fc1590830cd9
qldfhr123 <alfowko3258@naver.com>
2025-05-15 11:27:20 +0900

    프로젝트 초기 시작

---
+++ Project_JOB_Search/src/main/java/com/smhrd/controller/ConnController.java
@@ -0,0 +1,54 @@
+package com.smhrd.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.MediaType;
+
+import com.smhrd.model.ConnVO;
+
+@Controller
+public class ConnController {
+
+	@PostMapping("/ConnV")
+	public String Conn(@ModelAttribute ConnVO vo, Model model) {
+		int num1 = vo.getIn_num1();
+		int num2 = vo.getIn_num2();
+
+		// FastAPI로 JSON POST 요청
+		String url = "http://192.168.219.234:9001/calculate";
+
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		Map<String, Integer> requestBody = new HashMap<>();
+		requestBody.put("num1", num1);
+		requestBody.put("num2", num2);
+
+		HttpEntity<Map<String, Integer>> request = new HttpEntity<>(requestBody, headers);
+
+		ResponseEntity<Map> response = restTemplate.postForEntity(url, request, Map.class);
+		int result = (int) response.getBody().get("result");
+
+		model.addAttribute("result", result);
+		return "ConnV";
+	}
+
+	@RequestMapping("/Conn")
+	public String member() {
+
+		return "Conn";
+	}
+}
---
+++ Project_JOB_Search/src/main/java/com/smhrd/model/ConnVO.java
@@ -0,0 +1,16 @@
+package com.smhrd.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor 
+public class ConnVO {
+	
+	private int in_num1;
+	private int in_num2;
+
+
+}
