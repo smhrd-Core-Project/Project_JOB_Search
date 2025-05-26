aae3e3f6c2cd94739753a7a2c3cec0af7759003b
qldfhr123 <alfowk3258@naver.com>
2025-05-26 11:34:33 +0900

    마이페이지 재탐색시 수정

--- Project_JOB_Search/src/main/java/com/smhrd/database/SurveyMapper.java
+++ Project_JOB_Search/src/main/java/com/smhrd/database/SurveyMapper.java
@@ -3,6 +3,7 @@ package com.smhrd.database;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper
@@ -11,6 +12,14 @@ public interface SurveyMapper {
 	public List<String> getRandomSurveys();
 
 	public List<String> getRandomSurveysByGroup(String group);
+
+	 /**
+     * 재탐색 시 기존 로그를 업데이트 합니다.
+     * @param id                회원 아이디
+     * @param recombinedMajors  쉼표로 이은 추천 전공 문자열
+     */
+    void user_relog(@Param("id") String id,
+                      @Param("recombinedMajors") String recombinedMajors);
 
 	
 	
